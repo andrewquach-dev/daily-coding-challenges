76ae00741929b0ddf1798546c101f1dc3df1c3fa
Andrew Quach <andrewrquach@gmail.com>
2022-04-26 13:12:59 -0700

    ✨ added 2022_04_26 challenge

---
+++ 2022_04-26.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/55ecd718f46fba02e5000029/solutions/javascript
+
+function between(a, b) {
+  // your code here
+  arr = []
+  for (i = a; i <= b; i++) {
+    arr.push(i)
+  }
+  return arr
+}
