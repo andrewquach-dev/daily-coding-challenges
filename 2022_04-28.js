1c14a04d7a683b7e2eed42207a5f6d36f04eb854
Andrew Quach <andrewrquach@gmail.com>
2022-04-28 16:15:45 -0700

    ✨ added 2022_04_28 challenge

---
+++ 2022_04-28.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/57a5015d72292ddeb8000b31/solutions/javascript
+
+function isPalindrome(line) {
+    return (String(line) == String(line).split('').reverse().join('') )
+    
+  }
