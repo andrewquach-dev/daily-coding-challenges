ca3b7d616514b68dff9e0c3d7942ad02801aea74
Andrew Quach <andrewrquach@gmail.com>
2022-04-30 11:47:45 -0700

    ✨ added 2022_04_30 challenge

---
+++ 2022_04-30.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/56f695399400f5d9ef000af5/train/javascript
+
+function correctTail(body, tail) {
+
+  let sub = body.substr(body.length - (tail.length));
+
+  if (sub === tail) {
+    return true;
+  }
+  else {
+    return false;
+
+  }
+}
