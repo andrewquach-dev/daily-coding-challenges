d301cdd2d1de264f3ff72eb8747b6d232e294dbc
Andrew Quach <andrewrquach@gmail.com>
2022-05-12 16:49:01 -0700

    ✨ added 2022_05_12 challenge

---
+++ 2022_05-12.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/5808e2006b65bff35500008f/solutions/javascript
+
+function position(letter){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    return 'Position of alphabet: ' + (alphabet.indexOf(letter) + 1);
+  }
