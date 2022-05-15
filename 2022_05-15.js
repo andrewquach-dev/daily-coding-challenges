2add7d5f4d11be4d193ba2b4edb048487d13270f
Andrew Quach <andrewrquach@gmail.com>
2022-05-15 15:27:13 -0700

    ✨ added 2022_05_15 challenge

---
+++ 2022_05-15.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/582dafb611d576b745000b74/solutions/javascript
+
+var quote = function (fighter) {
+  // your code here
+  return (fighter.toLowerCase() === 'george saint pierre') ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
