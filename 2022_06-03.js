2412cdb455f60316fd4ec5f78207e5de5381640f
Andrew Quach <andrewrquach@gmail.com>
2022-06-03 14:45:52 -0700

    ✨ added 2022_06_03 challenge

---
+++ 2022_06-03.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/53951fff369894e4f10007a9/solutions/javascript/me/best_practice
+
+function double(array) {
+  // Use array.map() to return a new array with each value twice
+  // as large as the corresponding value in the passed in array.
+  return array.map(n => n * 2);
+}
