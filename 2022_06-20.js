2f0ed1053bd1b1ace6a96847facdf927086b0f8f
Andrew Quach <andrewrquach@gmail.com>
2022-06-21 16:33:18 -0700

    ✨ added 2022_06_21 challenge

--- 2022_06-20.js
+++ 2022_06-20.js
@@ -1,14 +1,6 @@
-//https://www.codewars.com/kata/580a4001d6df740d61000301/solutions/javascript?filter=me&sort=best_practice
+//https://www.codewars.com/kata/580a41b6d6df740d6100030c/solutions/javascript?filter=me&sort=best_practice
 
-function completeSeries(arr) {
-  // write your code here
-  let a = [];
-  if (arr.every((ele, i) => arr.lastIndexOf(ele) === i)) {
-    arr = arr.sort((a, b) => a - b);
-    for (let i = 0; i < arr[arr.length - 1] + 1; i++) {
-      a.push(i);
-    }
-    return a;
-  }
-  return [0];
+function alan(x) {
+  let stops = ['Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway'];
+  return stops.every(stop => x.includes(stop)) ? 'Smell my cheese you mother!' : 'No, seriously, run. You will miss it.';
 }
