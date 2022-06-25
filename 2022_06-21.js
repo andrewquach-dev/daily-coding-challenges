58c700f2ba8f612e9814e933f441f222b567ec8c
Andrew Quach <andrewrquach@gmail.com>
2022-06-24 19:01:52 -0700

    ✨ added 2022_06_24 challenge

--- 2022_06-21.js
+++ 2022_06-21.js
@@ -1,17 +1,5 @@
-//https://www.codewars.com/kata/535474308bb336c9980006f2/solutions/javascript?filter=me&sort=best_practice
+//https://www.codewars.com/kata/585b1fafe08bae9988000314/solutions/javascript?filter=me&sort=best_practice
 
-var greet = function (name) {
-  //P: name is a string
-
-  //R: string  with 'Hello ', name and '!'
-
-  //E(3): 
-  //"riley" --> "Hello Riley!"
-  //"JACK"  --> "Hello Jack!"
-
-  //P: 
-  //return 'Hello ' + name+ '!'
-
-  return `Hello ${name.charAt(0).toUpperCase() + name.slice(1, name.length).toLowerCase()}!`
-
-};
+function explode(s) {
+  return s.split('').map(s => Number(s)).map(n => n.toString().repeat(n)).join('');
+}
