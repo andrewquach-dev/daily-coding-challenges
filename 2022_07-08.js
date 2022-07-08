131f1fa649fd1e771acafd8b2dd1844d228085c3
Andrew Quach <andrewrquach@gmail.com>
2022-07-08 16:33:01 -0700

    ✨ added 2022_07_08 challenge

---
+++ 2022_07-08.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/6071ef9cbe6ec400228d9531/solutions/javascript
+
+const dotCalculator = (equation) => {
+  const operations = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '//': (a, b) => a / b,
+  };
+  const [left, operator, right] = equation.split(' ');
+  return '.'.repeat(operations[operator](left.length, right.length));
+}
