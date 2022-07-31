e841986572499f347ce2ec52daa31b2d60a8ad26
Andrew Quach <andrewrquach@gmail.com>
2022-07-31 15:08:31 -0700

    ✨ added 2022_07_31 challenge

---
+++ 2022_07-31.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/5c857bab20089d4fe65a7f5b/solutions/javascript?filter=me&sort=best_practice
+
+function clean(arr) {
+  return arr.filter((e) => e !== null || e !== undefined);
+}
