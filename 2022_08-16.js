8be4c76c2e1b303cc6814bccdbd541d35e2dc960
Andrew Quach <andrewrquach@gmail.com>
2022-08-16 11:45:25 -0700

    ✨ added 2022_08_16 challenge

---
+++ 2022_08-16.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/581351c40d8f13bc450008b8
+
+Array.prototype.last=function(){
+  return this[this.length-1]
+}
+
+Array.prototype.first=function(){
+  return this[0]
+}
