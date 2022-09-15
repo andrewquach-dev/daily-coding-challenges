23060e85933d679cd0152766a4b9e281cce49c5e
Andrew Quach <andrewrquach@gmail.com>
2022-09-15 13:18:37 -0700

    ✨ added 2022_09_16 challenge

---
+++ 2022_09-15.js
@@ -0,0 +1,34 @@
+//https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript
+
+function isPangram(string) {
+  //...
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  return alphabet.every((l) => string.toLowerCase().includes(l));
+}
