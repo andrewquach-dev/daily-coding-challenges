393f7af61e383105f555c4188548002e60d62b8f
Andrew Quach <andrewrquach@gmail.com>
2022-10-10 22:04:30 -0700

    ✨ added 2022_10_10 challenge

---
+++ 2022_10_10.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/55a13556ca4a6d0ab4000003/train/javascript
+
+var Person = function () {
+  var person = {
+    _name: "Leroy",
+    _friends: [],
+    fillFriends(f) {
+      this._friends.push(...f);
+    },
+  };
+  return person;
+};
