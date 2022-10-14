a6da97a52c9fd57415cd66d7a5e5a4b66b7f490f
Andrew Quach <andrewrquach@gmail.com>
2022-10-13 20:32:24 -0700

    ✨ added 2022_10_13 challenge

---
+++ 2022_10_13.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/5732b0351eb838d03300101d/train/javascript
+
+function blackAndWhite(arr) {
+  //coding here...
+  return !Array.isArray(arr)
+    ? "It's a fake array"
+    : arr.includes(5) && arr.includes(13)
+    ? "It's a black array"
+    : "It's a white array";
+}
