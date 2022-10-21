769ed60e58bbcc5148dddec9a9349e4f6480023c
Andrew Quach <andrewrquach@gmail.com>
2022-10-21 12:08:40 -0700

    ✨ added 2022_10_21 challenge

---
+++ 2022_10_21.js
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/is-subsequence/submissions/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isSubsequence = (s, t) => {
+  if (s.length === 0) return true;
+
+  let sPointer = 0;
+  let tPointer = 0;
+
+  while (sPointer < s.length && tPointer < t.length) {
+    if (s[sPointer] === t[tPointer]) sPointer++;
+
+    tPointer++;
+  }
+
+  return sPointer === s.length;
+};
