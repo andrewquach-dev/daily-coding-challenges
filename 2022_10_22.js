6f5bfee5b602ae16f6cb54415f8314d5113ec0e9
Andrew Quach <andrewrquach@gmail.com>
2022-10-22 14:39:55 -0700

    ✨ added 2022_10_22 challenge

---
+++ 2022_10_22.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/525e5a1cb735154b320002c8/train/javascript
+
+function triangular(n) {
+  let output = [];
+  if (n) {
+    for (let i = 0; i <= n; i++) {
+      output.push(i);
+    }
+  } else {
+    return 0;
+  }
+  return output.reduce((sum, current) => sum + current, 0);
+}
