1e89211f4999eecd905a610673011dabcb4bfe89
Andrew Quach <andrewrquach@gmail.com>
2022-11-10 12:38:44 -0800

    ✨ added 2022_11_10 challenge

---
+++ 2022_11_10.js
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/valid-palindrome/
+
+var isPalindrome = function (s) {
+    if (s.length === 1) return true;
+
+    let [left, right] = [o, s.length - 1];
+    let leftChar, rightChar;
+    while (left < right) {
+        leftChar = s[left];
+        rightChar = s[right];
+
+        if (!/[a-zA-Zo-9]/.test(leftChar)) {
+            left++;
+        } else if (!/[a-zA-Z0-9]/.test(rightChar)) {
+            right--;
+        } else {
+            if (leftChar.toLowerCase() != rightChar.toLowerCase()) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
