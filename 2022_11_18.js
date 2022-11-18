06d21b6adaf3e0da2f11b91a5e6353fda50bcdaa
Andrew Quach <andrewrquach@gmail.com>
2022-11-18 13:18:08 -0800

    ✨ added 2022_11_18 challenge

---
+++ 2022_11_18.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+var lengthOfLongestSubstring = function (s) {
+    const set = new Set();
+    let l = 0;
+    let max = 0;
+
+    for (let r = 0, sl = s.length; r < sl; r++) {
+        while (set.has(s[r])) {
+            set.delete(s[l])
+            l++;
+        }
+        set.add(s[r])
+        max = Math.max(max, set.size);
+    }
+    return max;
+}
