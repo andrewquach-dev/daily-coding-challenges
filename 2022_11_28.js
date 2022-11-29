8e53e1324067add63947f56f8aad63e4edd05d8c
Andrew Quach <andrewrquach@gmail.com>
2022-11-28 17:28:37 -0800

    ✨ added 2022_11_28 challenge

---
+++ 2022_11_28.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/528e95af53dcdb40b5000171/train/javascript
+
+
+function factorial(n) {
+  let ans = 1;
+  for (let i = ans; i <= n; i++) {
+    ans *= i
+  }
+  return n < 0 ? null : ans;
+}
