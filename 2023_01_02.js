2b9c607333bb8bbfc6d9daaa205de0e36d276419
Andrew Quach <andrewrquach@gmail.com>
2023-01-02 22:11:00 -0800

    ✨ added 2023_01_02 challenge

---
+++ 2023_01_02.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/5641275f07335295f10000d0/train/javascript
+
+function splitTheBill(x) {
+  let average = Object.values(x).reduce((a, b) => a + b, 0) / Object.values(x).length;
+  let arr = Object.keys(x);
+  let arr2 = Object.values(x).map((a, b) => (a - average));
+  return Object.assign(...arr.map((n, i) => ({ [n]: parseFloat((arr2[i]).toFixed(2)) })));
+}
