516e269c6589d727ea84bc60fbb74b5accba5d94
Andrew Quach <andrewrquach@gmail.com>
2023-01-03 14:41:06 -0800

    ✨ added 2023_01_03 challenge

---
+++ 2023_01_03.js
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/daily-temperatures/submissions/870760589/
+
+var dailyTemperatures = function(temperatures, stack = []) {
+  const days = Array(temperatures.length).fill(0);
+
+  for (let day = 0; day < temperatures.length; day++) {/* Time O(N + N) */
+      while (canShrink(stack, temperatures, day)) {    /* Time O(N + N) */
+          const prevColdDay = stack.pop();
+          const daysToWait = (day - prevColdDay);
+
+          days[prevColdDay] = daysToWait;              /* Ignore Space O(N) */
+      }
+
+      stack.push(day);                                 /* Space O(N) */
+  }
+
+  return days;
+};
+
+const canShrink = (stack, temperatures, day) => {
+  const previousDay = stack[stack.length - 1];
+  const [ prevTemperature, currTemperature ] = [ temperatures[previousDay], temperatures[day] ];
+  const isWarmer = prevTemperature < currTemperature;
+
+  return stack.length && isWarmer;
+}
