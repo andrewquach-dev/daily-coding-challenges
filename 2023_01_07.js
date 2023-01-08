176212de03b911b24216ad2c3252be0ccfe5d62e
Andrew Quach <andrewrquach@gmail.com>
2023-01-07 21:54:30 -0800

    ✨ added 2023_01_07 challenge

---
+++ 2023_01_07.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/58e0f0bf92d04ccf0a000010/train/javascript
+
+function lostSheep(friday, saturday, total) {
+  return total - (friday.reduce((acc, next) => acc + next, 0)) - (saturday.reduce((acc, next) => acc + next, 0))
+}
