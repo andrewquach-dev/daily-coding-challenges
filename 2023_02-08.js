38d176861466776f8e1eff096a13cf460c77612e
Andrew Quach <andrewrquach@gmail.com>
2023-02-08 13:11:23 -0800

    ✨ added 2023_02_08 challenge

---
+++ 2023_02-08.js
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/longest-consecutive-sequence/
+
+const longestConsecutive = (arr) => {
+    //get unique elms  
+    const S = new Set(arr);
+
+    //store the result
+    let max = 0;
+
+    // iterate array elements
+    for (let e of arr) {
+
+        // if the current element begins a new seq
+        if (!S.has(e - 1)) {
+
+            // tracks the length of subsequence
+            let len = 1;
+
+            // count all the elements of the subsequence
+            // e+1, e+2,...
+            while (S.has(e + len)) {
+                len++;
+            }
+
+            // get the max subsequence
+            max = Math.max(max, len);
+        }
+    }
+
+    return max;
+}
