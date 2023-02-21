648426d674753bdbb3e0563f198e05ceeaff462f
Andrew Quach <andrewrquach@gmail.com>
2023-02-21 12:52:38 -0800

    ✨ added 2023_02_21 challenge

---
+++ 2023_02-21.js
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/valid-parentheses/
+
+var isValid = function (s) {
+    let map = { '(': ')', '[': ']', '{': '}' };
+    let stack = [];
+    for (let ch of s) {
+        if (ch in map) {
+            //pushing opening bracket first
+            stack.push(map[ch]);
+        }
+        else if (stack.length > 0 && stack[stack.length - 1] == ch) {
+            //if we have a matching closing bracket to the one we pushed earlier, we simply pop it.
+            stack.pop();
+        }
+        else return false;
+    }
+    //if at last all values are poped i.e stack is empty, we have valid set of parentheses.
+    return stack.length == 0;
+};
