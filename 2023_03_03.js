8987ad8842fabfa63fa90498eac2c310b2c3c634
Andrew Quach <andrewrquach@gmail.com>
2023-03-04 18:33:07 -0800

    ✨ added 2023_03_04 challenge

--- 2023_03_03.js
+++ 2023_03_03.js
@@ -1,47 +1,13 @@
-//
+//https://leetcode.com/problems/linked-list-cycle/
 
-var mergeTwoLists = function (list1, list2) {
-    // Initialise a new LinkedList with a dummy ListNode
-    let newList = new ListNode(0);
-
-    // Maintain a reference to the head of the new LinkedList
-    let headOfNewList = newList;
-
-    // Whilst both of the passed in lists contain more elements
-    while (l1 && l2) {
-
-        // If li's value is smaller
-        if (l1.val < l2.val) {
-
-            // Add l1's value to the new list
-            newList.next = l1;
-
-            // Move l1 to its next element
-            l1 = l1.next;
-        } else {
-
-            // Add l2's value to the new list
-            newList.next = l2;
-
-            // Move l2 to its next element
-            l2 = l2.next;
+var hasCycle = function (head) {
+    let [slow, fast] = [head, head]
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) {
+            return true
         }
-
-        // Move into the next level of the LinkedList for the next iteration
-        newList = newList.next;
     }
-
-    // If l1 has run out of elements
-    if (!l1) {
-
-        // Append l2 to the new list
-        newList.next = l2;
-    } else {
-
-        // Append l1 to the new list
-        newList.next = l2;
-    }
-
-    return headOfNewList.next;
-
+    return false
 };
