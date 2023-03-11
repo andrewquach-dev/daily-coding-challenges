ada05366ffddb6360c101a54f3530a9aba2595ab
Andrew Quach <andrewrquach@gmail.com>
2023-03-10 16:01:27 -0800

    added 2023_03_10 challenge

---
+++ 2023_03_09.js
@@ -0,0 +1,9 @@
+//https://www.lintcode.com/problem/659/
+
+var encode = (strs, nonASCIICode = String.fromCharCode(257)) => {
+    return strs.join(nonASCIICode);/* Time O(N) | Ignore Auxillary Space O(N) */
+};
+
+var decode = (strs, nonASCIICode = String.fromCharCode(257)) => {
+    return strs.split(nonASCIICode);/* Time O(N) | Ignore Auxillary Space O(N) */
+};
