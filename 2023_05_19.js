02b02dacd5b33160fa383e911e39d4eee6ee0d0a
Andrew Quach <andrewrquach@gmail.com>
2023-05-19 15:32:11 -0700

    Daily update: 2023-05-19

---
+++ 2023_05_19.js
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/longest-increasing-subsequence/
+
+
+
+var lengthOfLIS = function (nums) {
+    const dp = (new Array(nums.length)).fill(1);
+
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[j] + 1, dp[i])
+            }
+        }
+    }
+    return Math.max(...dp);
+};
