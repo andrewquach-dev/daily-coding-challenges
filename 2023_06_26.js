f46dd84027761677945f90711b4e13dfe2c86fc6
Andrew Quach <andrewrquach@gmail.com>
2023-06-26 16:32:08 -0700

    Daily update: 2023-06-26

---
+++ 2023_06_26.js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/5adbc57f0774dbaa5600011b/train/javascript
+
+Map.prototype.reduce = function (reduceFn, acc) {
+  for (let [value, key] of this) {
+    acc = reduceFn(acc, key, value);
+  }
+  return acc;
+};
+
+const fizzBuzzReloaded = (start, stop, step, fns) =>
+  Array.from(
+    {
+      length: (stop - start) / step + 1,
+    },
+    (_, i) =>
+      fns.reduce(
+        (acc, fn, value) => (fn(start + i * step) ? acc + value : acc),
+        ""
+      ) || start + i * step
+  ).join` `;
