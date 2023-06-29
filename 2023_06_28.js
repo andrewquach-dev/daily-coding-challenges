52257099ea61adbce72214b097cd7ef656482ac5
Andrew Quach <andrewrquach@gmail.com>
2023-06-28 17:05:59 -0700

    Daily update: 2023-06-28

---
+++ 2023_06_28.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5c1ae703ba76f438530000a2
+
+function wordMesh(arr) {
+  let r = "";
+  for (let i = 0; i < arr.length - 1; i++) {
+    let t = (arr[i] + " " + arr[i + 1]).match(/(.+) \1/);
+    if (!t) return "failed to mesh";
+    r += t[1];
+  }
+  return r;
+}
