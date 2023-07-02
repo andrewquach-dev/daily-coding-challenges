b5ea3628f2af0508b5999f04d4a091be5a2a7091
Andrew Quach <andrewrquach@gmail.com>
2023-07-02 13:51:46 -0700

    Daily update: 2023-07-02

---
+++ 2023_07_02.js
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/56368f37d464c0a43c00007f/train/javascript
+
+const calculate = (a, op, b) => {
+  switch (op) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return b ? a / b : null;
+  }
+
+  return null;
+};
