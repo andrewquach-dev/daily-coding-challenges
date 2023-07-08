095acda301f8fbb436093d21587fb0b21c966830
Andrew Quach <andrewrquach@gmail.com>
2023-07-08 16:01:18 -0700

    Daily update: 2023-07-08

---
+++ 2023_07_08.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5a0b4dc2ffe75f72f70000ef
+
+function findChildren(santasList, children) {
+  let arr = [];
+  for (let item of santasList) {
+    for (let child of children) {
+      if (item === child && !arr.includes(item)) {
+        arr.push(item);
+      }
+    }
+  }
+  return arr.sort();
+}
