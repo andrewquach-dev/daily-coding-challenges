84897994e8909db2d1e9e74c5302771cfef5fa72
Andrew Quach <andrewrquach@gmail.com>
2023-08-02 14:30:01 -0700

    Daily update: 2023-08-02

---
+++ 2023_08_02.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5844a422cbd2279a0c000281/train/javascript
+
+function multi(arr) {
+  return arr.reduce((a, b) => a * b);
+}
+function add(arr) {
+  return arr.reduce((a, b) => a + b);
+}
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
