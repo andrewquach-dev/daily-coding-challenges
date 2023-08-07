19e253d25f6a38a5dd032331383d7f7a7924ed07
Andrew Quach <andrewrquach@gmail.com>
2023-08-06 17:19:00 -0700

    Daily update: 2023-08-06

---
+++ 2023_08_06.js
@@ -0,0 +1,22 @@
+//https://www.codewars.com/kata/6167e70fc9bd9b00565ffa4e
+
+function barista(coffees) {
+  //TODO: Maths are cool!
+
+  // 0 + 2 + 4 + 6 + 8
+  // 0 + 2(1) + 2(2) + 2(3) + 2(4)
+
+  var sum = 0;
+  var coffees = coffees.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(coffees);
+  coffees.reduce((previousValue, currentValue, currentIndex) => {
+    var waitTime =
+      currentIndex < 1 ? currentValue : previousValue + currentValue + 2;
+    console.log(waitTime);
+    sum += waitTime;
+    return waitTime;
+  }, 0);
+  return sum;
+}
