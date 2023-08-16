4c8643afc88f3621bf2c0f9271766723e7869bdd
Andrew Quach <andrewrquach@gmail.com>
2023-08-16 16:57:01 -0700

    Daily update: 2023-08-16

---
+++ 2023_08_16.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
+
+snail = function (array) {
+    var result;
+    while (array.length) {
+        // Steal the first row.
+        result = (result ? result.concat(array.shift()) : array.shift());
+        // Steal the right items.
+        for (var i = 0; i < array.length; i++)
+            result.push(array[i].pop());
+        // Steal the bottom row.
+        result = result.concat((array.pop() || []).reverse());
+        // Steal the left items.
+        for (var i = array.length - 1; i >= 0; i--)
+            result.push(array[i].shift());
+    }
+    return result;
+}
