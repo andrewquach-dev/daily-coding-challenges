7a531a6f1179d9387396e8a455b948600f24110b
Andrew Quach <andrewrquach@gmail.com>
2023-09-05 17:24:23 -0700

    Daily update: 2023-09-05

---
+++ 2023_09_05.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/5b728f801db5cec7320000c7
+
+function solve(s, k) {
+    const abc = "abcdefghijklmnopqrstuvwxyz"
+
+    for (let i = 0; i < abc.length; i += 1) {
+        while (s.includes(abc[i]) && k > 0) {
+            s = s.replace(abc[i], "")
+            k -= 1
+        }
+    }
+
+    return s
+}
