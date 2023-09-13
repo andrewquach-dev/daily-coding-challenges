7ed7bcbbcbee325548cb0f064edf17d57f8a286b
Andrew Quach <andrewrquach@gmail.com>
2023-09-12 18:11:16 -0700

    Daily update: 2023-09-12

---
+++ 2023_09_12.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/56dbe7f113c2f63570000b86
+
+function rot(s) {
+    return s.split("").reverse().join("");
+}
+
+function selfieAndRot(s) {
+    return (s = s.replace(/.+/g, t => t + t.replace(/./g, "."))) + "\n" + rot(s);
+}
+
+function oper(fn, s) {
+    return fn(s);
+}
