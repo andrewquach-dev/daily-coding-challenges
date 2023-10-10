9e915fb903b5286f2cd09dde9dd28c86ff069394
Andrew Quach <andrewrquach@gmail.com>
2023-10-09 19:24:24 -0700

    Daily update: 2023-10-09

---
+++ 2023_10_09.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/57d532d2164a67cded0001c7/train/javascript
+
+function histogram(results) {
+    var histogram = '';
+
+    for (var i = 5; i >= 0; --i)
+        histogram += (i + 1) + '|' + '#'.repeat(results[i]) + (results[i] > 0 ? ' ' + results[i] : '') + "\n";
+
+    return histogram;
+}
