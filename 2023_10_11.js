dcaaf661c0bf4c51dba4cfd99fc0646212b8a415
Andrew Quach <andrewrquach@gmail.com>
2023-10-11 16:34:57 -0700

    Daily update: 2023-10-11

---
+++ 2023_10_11.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/588422ba4e8efb583d00007d/train/javascript
+
+function lateRide(n) {
+    let hours = Math.floor(n / 60)
+    let minutes = (n - hours * 60)
+    return (`${hours}${minutes}`).split('').reduce((a, b) => a + +(b), 0)
+}
