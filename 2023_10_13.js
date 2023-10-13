bc4eec8cd53c6c9721d9f1bc496284fa99b5fe00
Andrew Quach <andrewrquach@gmail.com>
2023-10-13 14:05:29 -0700

    Daily update: 2023-10-13

---
+++ 2023_10_13.js
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/pascals-triangle/
+
+var generate = function (numRows) {
+    const res = [[1]];
+
+    for (let i = 1; i < numRows; i++) {
+        res[i] = [];
+        for (let k = 0; k < i + 1; k++) {
+            res[i][k] = (res[i - 1][k] || 0) + (res[i - 1][k - 1] || 0);
+        }
+    }
+    return res;
+}
