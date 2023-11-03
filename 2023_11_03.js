860e95dab1a56028978e08058d864c467617af95
Andrew Quach <andrewrquach@gmail.com>
2023-11-03 16:32:38 -0700

    Daily update: 2023-11-03

---
+++ 2023_11_03.js
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/unique-email-addresses/
+
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+var numUniqueEmails = function (emails) {
+
+    let uniqueEmails = new Set();  // to store unique email addresses
+
+    for (let email of emails) {
+
+        let [local, domain] = email.split('@');  // split local and domain parts
+
+        local = local.replace(/\./g, '');  // remove dots from local part
+
+        let plusIndex = local.indexOf('+');  // find index of plus sign
+
+        if (plusIndex !== -1) {
+
+            local = local.substring(0, plusIndex); // remove everything after plus sign
+        }
+
+        uniqueEmails.add(local + '@' + domain); // add modified email to set
+    }
+
+    return uniqueEmails.size; // return number of unique email addresses
+
+};
