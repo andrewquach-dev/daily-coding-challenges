aa60f0335a87ece5dcfcebd61a3d8bb71f5164eb
Andrew Quach <andrewrquach@gmail.com>
2023-11-08 17:41:08 -0800

    Daily update: 2023-11-08

---
+++ 2023_11_08.js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/573992c724fc289553000e95
+
+
+function smallest(n) {
+    let row = String(n).split("");
+    let min = [n, 0, 0],
+        test = [];
+
+    for (var i = 0, length = row.length; i < length; i++) {
+        for (var j = 0; j < length; j++) {
+            test = row.slice(0, i).concat(row.slice(i + 1));
+
+            if (Number(test.slice(0, j).concat(row[i], test.slice(j)).join("")) < min[0]) {
+                min = [Number(test.slice(0, j).concat(row[i], test.slice(j)).join("")), i, j];
+            }
+        }
+    }
+
+    return min;
+}
