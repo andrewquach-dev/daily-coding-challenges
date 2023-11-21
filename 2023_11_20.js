0a8ee3156f472dda9b11e31b6130718a120d8719
Andrew Quach <andrewrquach@gmail.com>
2023-11-20 20:46:12 -0800

    Daily update: 2023-11-20

---
+++ 2023_11_20.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5262119038c0985a5b00029f
+
+
+function isPrime(num) {
+    if (num < 2) return false;
+    const limit = Math.sqrt(num);
+    for (let i = 2; i <= limit; ++i) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
