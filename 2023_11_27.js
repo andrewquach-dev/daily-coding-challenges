505792cce0265679cc9a57d366a0fbbbe5ad5a8e
Andrew Quach <andrewrquach@gmail.com>
2023-11-27 20:15:42 -0800

    Daily update: 2023-11-27

---
+++ 2023_11_27.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/52685f7382004e774f0001f7
+function humanReadable(seconds) {
+    var pad = function (x) { return (x < 10) ? "0" + x : x; }
+    return pad(parseInt(seconds / (60 * 60))) + ":" +
+        pad(parseInt(seconds / 60 % 60)) + ":" +
+        pad(seconds % 60)
+}
