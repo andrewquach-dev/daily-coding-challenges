66a7f831337b2a0a82964ef3cbf8b9718b2c2395
Andrew Quach <andrewrquach@gmail.com>
2023-12-04 17:40:43 -0800

    Daily update: 2023-12-04

---
+++ 2023_12_04.js
@@ -0,0 +1,16 @@
+const fib = (n) => {
+
+    const table = Array(n + 1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i <= n; i++) {
+        table[i + 1] += table[i]
+        table[i + 2] += table[i]
+    }
+    return table[n];
+
+
+}
+
+//fib tabulation
+//has to have array that's 1 size greater than argument
+//add value to next two numbers
