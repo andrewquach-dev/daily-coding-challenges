fad36f00689c0567a3576165d1c79820b475dcc3
Andrew Quach <andrewrquach@gmail.com>
2023-12-19 18:45:27 -0800

    Daily update: 2023-12-19

---
+++ 2023_12_19.js
@@ -0,0 +1,12 @@
+const canSum = (targetSum, numbers) => {
+    const table = Array(targetSum + 1).fill(false);
+    table[0] = true;
+    for (let i = 0; i <= targetSum; i++) {
+        if (table[i] === true) {
+            for (let num of numbers) {
+                table[i + num] = true;
+            }
+        }
+    }
+    return table[targetSum];
+}
