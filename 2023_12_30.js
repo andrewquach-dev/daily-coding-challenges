66e599d295eddf1f605adf3d7275fac7556e23ed
Andrew Quach <andrewrquach@gmail.com>
2023-12-30 20:48:52 -0800

    Daily update: 2023-12-30

---
+++ 2023_12_30.js
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/5277c8a221e209d3f6000b56
+
+function validBraces(braces) {
+    var matches = { '(': ')', '{': '}', '[': ']' };
+    var stack = [];
+    var currentChar;
+
+    for (var i = 0; i < braces.length; i++) {
+        currentChar = braces[i];
+
+        if (matches[currentChar]) { // opening braces
+            stack.push(currentChar);
+        } else { // closing braces
+            if (currentChar !== matches[stack.pop()]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0; // any unclosed braces left?
+}
