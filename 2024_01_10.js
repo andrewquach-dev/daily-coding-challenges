52b4675011670c4f8b50e97e0aaa09fe00d77586
Andrew Quach <andrewrquach@gmail.com>
2024-01-10 17:04:37 -0800

    Daily update: 2024-01-10

---
+++ 2024_01_10.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/559e3224324a2b6e66000046
+
+function sumin(n) {
+    return n * n * n / 3 + n * n / 2 + n / 6;
+}
+function sumax(n) {
+    return 2 * n * n * n / 3 + n * n / 2 - n / 6;
+}
+function sumsum(n) {
+    return n * n * n + n * n;
+}
