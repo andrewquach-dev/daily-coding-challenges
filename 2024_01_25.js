7f2ab0a9444aa307b8ccce3d9e52f730fc73a8fa
Andrew Quach <andrewrquach@gmail.com>
2024-01-25 21:22:44 -0800

    Daily update: 2024-01-25

---
+++ 2024_01_25.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/59b0a6da44a4b7080300008a
+
+function to24hourtime(hour, minute, period) {
+    if (period === "am" && hour === 12) {
+        hour = 0;
+    } else if (period === "pm" && hour !== 12) {
+        hour += 12;
+    }
+    return hour.toString().padStart(2, "0") + minute.toString().padStart(2, "0");
+}
