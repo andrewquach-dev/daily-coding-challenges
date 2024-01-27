331bb4cf20220547b7f704d4bd8154ed57622261
Andrew Quach <andrewrquach@gmail.com>
2024-01-26 17:52:38 -0800

    Daily update: 2024-01-26

---
+++ 2024_01_26.js
@@ -0,0 +1,6 @@
+// /https://www.codewars.com/kata/5516ab668915478845000780
+
+function reverseByCenter(s) {
+    var n = s.length / 2
+    return s.slice(Math.ceil(n)) + s.slice(Math.floor(n), Math.ceil(n)) + s.slice(0, Math.floor(n))
+}
