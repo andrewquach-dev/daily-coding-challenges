9aa8434acf919d6c267f4aad65e2651ce5b8e682
Andrew Quach <andrewrquach@gmail.com>
2024-03-06 19:51:16 -0800

    Daily update: 2024-03-06

---
+++ 2024_03_06.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/55beec7dd347078289000021
+
+function Node(data) {
+    this.data = data
+    this.next = null
+}
+
+function length(head) {
+    return head ? 1 + length(head.next) : 0
+}
+
+function count(head, data) {
+    if (!head) return 0
+    return (head.data === data ? 1 : 0) + count(head.next, data)
+}
