de34d2626c5fdac24b0869a2dd1cb2b670154ca8
Andrew Quach <andrewrquach@gmail.com>
2024-03-27 17:02:12 -0700

    Daily update: 2024-03-27

---
+++ 2024_03_27.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+var maxProfit = function (prices) {
+    let [l, r, max] = [0, 1, 0];
+
+    while (r < prices.length) {
+        if (prices[l] < prices[r]) {
+            let profit = prices[r] - prices[l]
+            max = Math.max(max, profit);
+        } else {
+            l = r
+        }
+        r += 1;
+    }
+
+    return max;
+}
