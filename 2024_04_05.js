be371d9dfdff00d4e3c5fd8132a24b805e9bff10
Andrew Quach <andrewrquach@gmail.com>
2024-04-05 19:15:45 -0700

    Daily update: 2024-04-05

---
+++ 2024_04_05.js
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/valid-palindrome/description/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    //s is string
+    //boolean
+    //"A man, a plan, a canal: Panama" true
+    //"race a car" false
+    //" " true
+
+    //strip it  
+    //basecase
+    //loop through stripped s until halfway point
+    //if chars are equal continue else return false
+    //return true
+
+    s = s.split('').map(ch => ch.toLowerCase()).join('').replace(/[^a-zA-Z0-9]/g, '')
+
+    if (s == "") return true;
+
+    for (let i = 0; i < s.length / 2; i++) {
+        if (s[i] !== s[s.length - 1 - i]) return false
+    }
+
+    return true
+};
