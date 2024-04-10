ca4c48f52130777656734d31fd473bc0334563fb
Andrew Quach <andrewrquach@gmail.com>
2024-04-09 17:07:06 -0700

    Daily update: 2024-04-09

---
+++ 2024_04_09.js
@@ -0,0 +1,58 @@
+//https://leetcode.com/problems/implement-queue-using-stacks/solutions/4966096/beats-94-04-of-users-with-javascript-memory-usage/
+
+
+var MyQueue = function () {
+    this.stack1 = []
+    this.stack2 = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+    this.stack1.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+    if (this.stack2.length == 0) {
+
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop())
+        }
+    }
+    return this.stack2.pop() || null
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+    if (this.stack2.length === 0) {
+
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop())
+        }
+    }
+    return this.stack2[this.stack2.length - 1] || null
+
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+    return this.stack1.length == 0 && this.stack2.length == 0
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
