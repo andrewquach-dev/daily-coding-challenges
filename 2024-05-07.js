332ef694e7716c7213adaf3dd57060802090b59f
Andrew Quach <andrewrquach@gmail.com>
2024-05-07 12:32:23 -0700

    Daily update: 2024-05-07

---
+++ 2024-05-07.js
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    const answer = [];
+
+    while (left <= right) {
+        const leftSqr = Math.pow(nums[left], 2);
+        const rightSqr = Math.pow(nums[right], 2);
+
+        if (leftSqr > rightSqr) {
+            answer.push(leftSqr);
+            left++;
+        } else {
+            answer.push(rightSqr);
+            right--;
+        }
+    }
+    return answer.reverse();
+};
